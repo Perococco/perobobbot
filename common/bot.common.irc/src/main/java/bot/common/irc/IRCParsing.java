2eb63d78bff20be9625380ad6ba94e47f14c3a76
perococco <perococco.twitch@gmail.com>
2020-05-02 20:59:29 +0200

    Add Javadoc to IRCParsing.java

--- common/bot.common.irc/src/main/java/bot/common/irc/IRCParsing.java
+++ common/bot.common.irc/src/main/java/bot/common/irc/IRCParsing.java
@@ -13,18 +13,33 @@ import java.util.stream.Stream;
 @Builder(builderClassName = "Builder")
 public class IRCParsing {
 
+    /**
+     * The original message used by the parser
+     */
     @NonNull
     private final String rawMessage;
 
+    /**
+     * A map of tags
+     */
     @NonNull
     private final ImmutableMap<String,Tag> tags;
 
+    /**
+     * The optional prefix of the IRC message
+     */
     @Getter(AccessLevel.NONE)
     private final Prefix prefix;
 
+    /**
+     * The command of the IRC message
+     */
     @NonNull
     private final String command;
 
+    /**
+     * The parametres of the IRC message
+     */
     @NonNull
     private final Params params;
 
@@ -33,11 +48,20 @@ public class IRCParsing {
         return Optional.ofNullable(prefix);
     }
 
+    /**
+     * @return the last parameter of the IRC message.
+     * @throws IndexOutOfBoundsException if there is no such parameter
+     */
     @NonNull
     public String lastParameter() {
         return params.lastParameter();
     }
 
+    /**
+     * The last parameter split in token
+     * @param sep
+     * @return
+     */
     @NonNull
     public Stream<String> splitLastParameter(@NonNull String sep) {
         return Stream.of(lastParameter().split(sep));
