9d43c57a828cf4c6caa8d7195eb487e4cd3e1681
perococco <perococco.twitch@gmail.com>
2020-05-17 23:42:16 +0200

    Remove unused static variables

--- backend/data/perobobbot.data.domain/src/main/java/perobobbot/data/domain/UserRole.java
+++ backend/data/perobobbot.data.domain/src/main/java/perobobbot/data/domain/UserRole.java
@@ -16,15 +16,12 @@ import javax.persistence.*;
 @Table(name = "USER_ROLE")
 public class UserRole extends SimplePersistentObject {
 
-    public static final String COLUMN_NAME__USER_ID = "USER_ID";
-    public static final String COLUMN_NAME__ROLE_ID = "ROLE_ID";
-
     @ManyToOne
-    @JoinColumn(name = COLUMN_NAME__USER_ID, foreignKey = @ForeignKey(name=("FK_USER_ROLE__USER")), insertable = false, updatable = false)
+    @JoinColumn(name = "USER_ID", foreignKey = @ForeignKey(name=("FK_USER_ROLE__USER")), insertable = false, updatable = false)
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = COLUMN_NAME__ROLE_ID, foreignKey = @ForeignKey(name=("FK_USER_ROLE__ROLE")), insertable = false, updatable = false)
+    @JoinColumn(name = "ROLE_ID", foreignKey = @ForeignKey(name=("FK_USER_ROLE__ROLE")), insertable = false, updatable = false)
     private Role role;
 
     protected UserRole() {
