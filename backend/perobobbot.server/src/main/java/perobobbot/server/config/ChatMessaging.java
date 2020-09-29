e5ab9a8a808e75586afd893c0d2dcc5b2cad7942
perococco <perococco.twitch@gmail.com>
2020-09-29 22:12:41 +0200

    remove debug trace

--- backend/perobobbot.server/src/main/java/perobobbot/server/config/ChatMessaging.java
+++ backend/perobobbot.server/src/main/java/perobobbot/server/config/ChatMessaging.java
@@ -1,13 +1,10 @@
 package perobobbot.server.config;
 
-import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.messaging.MessageChannel;
-import perobobbot.common.lang.MessageContext;
 
 @Configuration
 @EnableIntegration
@@ -17,11 +14,5 @@ public class ChatMessaging {
     public MessageChannel chatChannel() {
         return new PublishSubscribeChannel();
     }
-
-    @ServiceActivator(inputChannel = "chatChannel")
-    public void trace(@NonNull MessageContext messageContext) {
-        System.out.println(messageContext.getContent());
-    }
-
 
 }
