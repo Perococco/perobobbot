343386dc3ef6d86f416b3ad45e127d983c003383
perococco <perococco.twitch@gmail.com>
2020-10-20 19:28:35 +0200

    Add badge values

--- backend/engine/twitch/perobobbot.twitch.chat/src/main/java/perobobbot/twitch/chat/BadgeName.java
+++ backend/engine/twitch/perobobbot.twitch.chat/src/main/java/perobobbot/twitch/chat/BadgeName.java
@@ -4,5 +4,8 @@ public class BadgeName {
     public static final String SUBSCRIBER = "subscriber";
     public static final String MODERATOR = "moderator";
     public static final String BROADCASTER = "broadcaster";
+    public static final String STAFF = "staff";
+    public static final String ADMIN = "admin";
+    public static final String GLOBAL_MOD = "global_mod";
 
 }
