5f18574192bc32497389a04446b3ecc6a4e5e610
perococco <perococco.twitch@gmail.com>
2020-11-12 09:47:26 +0100

    add javadoc to Game

--- backend/extensions/perobobbot.puckwar/src/main/java/perobobbot/puckwar/physic/Game.java
+++ backend/extensions/perobobbot.puckwar/src/main/java/perobobbot/puckwar/physic/Game.java
@@ -17,28 +17,58 @@ import java.util.function.Predicate;
 public class Game {
 
     public static @NonNull Game create(@NonNull OverlaySize overlaySize, int puckSize) {
-        final var initialPosition = Vector2D.of(0, overlaySize.getHeight()*0.5);
-        final UnaryOperator1<Vector2D> velocityModifier = v -> v.scale(5);
-        return new Game(puckSize,initialPosition,velocityModifier,new OutsiderPredicate(overlaySize));
+        final var initialPosition = Vector2D.of(0, overlaySize.getHeight() * 0.5);
+        return new Game(puckSize,
+                        initialPosition,
+                        v -> v.scale(5),
+                        new OutsiderPredicate(overlaySize));
     }
 
+    /**
+     * The default size of the pucks
+     */
     private final @NonNull int puckSize;
 
+    /**
+     * The initial position of all pucks
+     */
     private final @NonNull Vector2D initialPosition;
 
+    /**
+     * A modifier of the velocity so that the positive velocity are directed to the center of the overlay
+     */
     private final @NonNull UnaryOperator1<Vector2D> velocityModifier;
 
+    /**
+     * Predicate to test if a puck is outside of the overlay drawing region
+     */
     private final @NonNull Predicate<Puck> isOutsideGameRegion;
 
+    /**
+     * The throw that are not included in the game yet
+     */
+    private final BlockingDeque<Throw> pendingThrows = new LinkedBlockingDeque<>();
+
+    /**
+     * The list of active puck in the game
+     */
     private final List<Puck> pucks = new ArrayList<>(256);
 
-    private final BlockingDeque<Throw> pendingThrow = new LinkedBlockingDeque<>();
 
-
+    /**
+     * add a throw that will be added at the next rendering frame
+     *
+     * @param puckThrow the information about the throw
+     */
     public void addThrow(@NonNull Throw puckThrow) {
-        this.pendingThrow.add(puckThrow.modifyVelocity(velocityModifier));
+        this.pendingThrows.add(puckThrow.modifyVelocity(velocityModifier));
     }
 
+    /**
+     * Update the position of the pucks in the game
+     *
+     * @param dt the delta in time since the last update
+     */
     public void updateGame(double dt) {
         this.updatePuckPositions(dt);
         this.removeOutsiders();
@@ -54,21 +84,21 @@ public class Game {
     }
 
     private void addPendingThrowToPuckList() {
-        if (pendingThrow.isEmpty()) {
+        if (pendingThrows.isEmpty()) {
             return;
         }
-        final List<Throw> puckThrows = new ArrayList<>(pendingThrow.size()+10);
-        pendingThrow.drainTo(puckThrows);
-        puckThrows.stream()
-                  .map(t -> t.createPuck(initialPosition, puckSize))
-                  .forEach(pucks::add);
+        final List<Throw> retrievedPuckThrows = new ArrayList<>(pendingThrows.size() + 10);
+        pendingThrows.drainTo(retrievedPuckThrows);
+        retrievedPuckThrows.stream()
+                           .map(thrw -> thrw.createPuck(initialPosition, puckSize))
+                           .forEach(pucks::add);
     }
-
 
     public void draw(OverlayRenderer overlayRenderer) {
         pucks.forEach(p -> p.drawWith(overlayRenderer));
     }
 
-    public void dispose() {}
+    public void dispose() {
+    }
 
 }
