dbe1590beddc081aa653ffeff2036b73bba9083f
perococco <perococco.twitch@gmail.com>
2020-12-09 16:02:47 +0100

    remove useless @NonNull on primitive

--- backend/perobobbot.physics/src/main/java/perobobbot/physics/GravityEffect.java
+++ backend/perobobbot.physics/src/main/java/perobobbot/physics/GravityEffect.java
@@ -5,24 +5,24 @@ import lombok.NonNull;
 public enum GravityEffect {
     ATTRACTOR(){
         @Override
-        public double applyEffect(@NonNull double gravitationalFactor) {
+        public double applyEffect(double gravitationalFactor) {
             return gravitationalFactor;
         }
     },
     REPULSOR() {
         @Override
-        public double applyEffect(@NonNull double gravitationalFactor) {
+        public double applyEffect(double gravitationalFactor) {
             return -gravitationalFactor;
         }
     },
     NONE() {
         @Override
-        public double applyEffect(@NonNull double gravitationalFactor) {
+        public double applyEffect(double gravitationalFactor) {
             return 0;
         }
     },
     ;
 
-    public abstract double applyEffect(@NonNull double gravitationalFactor);
+    public abstract double applyEffect(double gravitationalFactor);
 
 }
