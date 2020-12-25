0e0f2007c7d60e3474877000180e302fa59024ba
perococco <perococco.twitch@gmail.com>
2020-12-25 23:41:18 +0100

    fix welcome page for dashboard

--- extensions/perobobbot.dashboard-svelte/java-src/main/java/perobobbot/frontend/spring/FrontEndConfig.java
+++ extensions/perobobbot.dashboard-svelte/java-src/main/java/perobobbot/frontend/spring/FrontEndConfig.java
@@ -2,20 +2,28 @@ package perobobbot.frontend.spring;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import perobobbot.lang.Packages;
 
 @Component
 public class FrontEndConfig implements WebMvcConfigurer {
 
+    public static final String CONTEXT = "/dashboard";
 
     public static Packages provider() {
         return Packages.with("[Extension] Frontend", FrontEndConfig.class);
     }
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController(CONTEXT).setViewName("redirect:"+CONTEXT+"/index.html");
+        registry.addViewController(CONTEXT+"/").setViewName("redirect:"+CONTEXT+"/index.html");
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/dashboard/**")
+        registry.addResourceHandler(CONTEXT+"/**")
                 .addResourceLocations("classpath:/dashboard-svelte/public/");
     }
 
