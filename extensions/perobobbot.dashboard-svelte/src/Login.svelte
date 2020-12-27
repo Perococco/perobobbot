c44c58f3885a17aa8156aa23c23023629380052e
perococco <perococco.twitch@gmail.com>
2020-12-27 19:24:07 +0100

    add tailwind class to Login form

--- extensions/perobobbot.dashboard-svelte/src/Login.svelte
+++ extensions/perobobbot.dashboard-svelte/src/Login.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import * as Authenticator from "./authenticator";
-    import {push, pop, replace} from 'svelte-spa-router';
-
+    import {replace} from 'svelte-spa-router';
+    import "./css/Tailwind.css";
 
     let login: string = "";
     let password: string = "";
@@ -21,15 +21,21 @@
 </style>
 
 
-<div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" bind:value={login} placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" bind:value={password} placeholder="Enter Password" name="psw" required>
-
-    <button type="submit" on:click|preventDefault={submitForm}>Login</button>
-    <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
+<div class="bg-blue-100">
+    <div class="container flex justify-center mt-40 mx-auto">
+        <form class="bg-blue-300 w-40 p-4 shadow-2xl">
+            <div class="p-4">
+                <input type="text" bind:value={login} placeholder="Enter Username" name="uname" required>
+            </div>
+            <div class="p-3">
+                <input type="password" bind:value={password} placeholder="Enter Password" name="psw" required>
+            </div>
+            <div class="p-3">
+                <button type="submit" on:click|preventDefault={submitForm}>Login</button>
+                <label>
+                    <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+            </div>
+        </form>
+    </div>
 </div>
