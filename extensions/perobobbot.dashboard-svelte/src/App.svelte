96e3a4bb558063cdf67b1b2bbf742161f1861588
perococco <perococco.twitch@gmail.com>
2020-12-27 15:25:06 +0100

    clean up App.svelte and reintroducte tailwind css

--- extensions/perobobbot.dashboard-svelte/src/App.svelte
+++ extensions/perobobbot.dashboard-svelte/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang='typescript'>
-    // import './TailwindStyles.svelte';
+    import './TailwindStyles.svelte';
 
     import {onMount, onDestroy} from 'svelte';
     import Router, {link} from 'svelte-spa-router'
@@ -11,22 +11,16 @@
 
     let authenticated_user: Optional<User> = Optional.empty();
 
-    const unsubscriber = Store.authentication.subscribe(o => {
-            console.log("New user : " + o.map(u => u.login).orElseGet("No user"))
-            authenticated_user = o
-        }
-    );
+    const unsubscriber = Store.authentication.subscribe(o => authenticated_user = o);
 
     onMount(() => Authenticator.initialize())
-    onDestroy(unsubscriber)
+    onDestroy(() => unsubscriber())
 
     const routes = new Map();
 
     routes.set("/login", wrap({
         asyncComponent: () => import('./Login.svelte'),
     }));
-
-
     routes.set(/^\/(home)?/, wrap({
         asyncComponent: () => import('./Home.svelte'),
         userData: {
@@ -44,10 +38,6 @@
             .ifPresent(r => replace(r))
     }
 
-    function logout():void {
-        Authenticator.logout();
-    }
-
 </script>
 
 <style>
@@ -57,7 +47,7 @@
     <div>
         <a href="/login" use:link>Login</a>
         <a href="/home" use:link>Home</a>
-        <a on:click|preventDefault={logout}>Logout</a>
+        <a href="/logout" on:click|preventDefault={Authenticator.logout()}>Logout</a>
     </div>
     <Router {routes} on:conditionsFailed={onRouteDenied}/>
 </div>
