579c26a5e38b6a241854ec6714d10f71eb10859c
perococco <perococco.twitch@gmail.com>
2020-12-30 20:45:26 +0100

    Display login on header of home

--- extensions/perobobbot.dashboard-svelte/src/routes/Home.svelte
+++ extensions/perobobbot.dashboard-svelte/src/routes/Home.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-    import {onMount} from "svelte";
+    import {onMount, onDestroy} from "svelte";
     import {styles} from "../stores/styles";
+    import {authentication} from "../stores/stores";
+    import {Authentication} from "../types/authentication";
+
+    let auth = Authentication.none();
+    const unsub = authentication.subscribe(a => auth=a);
+
+    $: login = auth.getUser().map(u => u.login).orElse("?")
 
     onMount(() => styles.update(g => g.withBackgroundForRoute("/home")));
+    onDestroy(unsub);
 </script>
 
 <style>
@@ -12,7 +20,7 @@
 <div class="full-screen">
     <div class="relative w-full">
         <div class="absolute top-0 bg-black bg-opacity-30 h-full w-full"></div>
-        <div class="relative top 0">Ah</div>
+        <div class="relative top 0 text-white">{login}</div>
     </div>
     <p>Secured Component</p>
 </div>
