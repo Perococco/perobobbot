d8e513d3b7dcc02a8c98c346cee22dcd3308f80a
perococco <perococco.twitch@gmail.com>
2021-01-23 19:59:49 +0100

    add frontfx module. remove unused classes

--- lib/perobobbot.lang/src/main/java/perobobbot/lang/Mutation.java
+++ lib/perobobbot.lang/src/main/java/perobobbot/lang/Mutation.java
@@ -1,6 +1,7 @@
 package perobobbot.lang;
 
 import lombok.NonNull;
+import perobobbot.lang.fp.Function1;
 
 public interface Mutation<S> {
 
@@ -8,7 +9,14 @@ public interface Mutation<S> {
         return t -> t;
     }
 
-
     @NonNull
     S mutate(@NonNull S state);
+
+    default @NonNull Mutation<S> then(@NonNull Mutation<S> after) {
+        return s -> after.mutate(this.mutate(s));
+    }
+
+    default @NonNull <T> Operator<S,T> thenGet(@NonNull Function1<? super S, ? extends T> action) {
+        return Operator.mutatorGetter(this,action);
+    }
 }
