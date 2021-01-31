d676e1505c54ff15c35302fb0f9ecc98b00ee8e2
perococco <perococco.twitch@gmail.com>
2021-01-31 18:16:31 +0100

    Add sandbox extension for testing

---
+++ extensions/perobobbot.sandbox/src/main/java/perobobbot/sandbox/SandboxOverlay.java
@@ -0,0 +1,13 @@
+package perobobbot.sandbox;
+
+import lombok.NonNull;
+import perobobbot.overlay.api.OverlayClient;
+import perobobbot.overlay.api.OverlayIteration;
+
+public class SandboxOverlay implements OverlayClient {
+
+    @Override
+    public void render(@NonNull OverlayIteration iteration) {
+        //TODO
+    }
+}
