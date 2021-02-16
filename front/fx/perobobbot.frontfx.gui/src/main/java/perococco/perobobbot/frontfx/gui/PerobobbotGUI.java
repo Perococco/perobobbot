ced61eaaa764fe3430c12897a569722c24859b1a
perococco <perococco.twitch@gmail.com>
2021-02-16 11:59:33 +0100

    Add configuration to application state

--- front/fx/perobobbot.frontfx.gui/src/main/java/perococco/perobobbot/frontfx/gui/PerobobbotGUI.java
+++ front/fx/perobobbot.frontfx.gui/src/main/java/perococco/perobobbot/frontfx/gui/PerobobbotGUI.java
@@ -2,9 +2,12 @@ package perococco.perobobbot.frontfx.gui;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import perobobbot.frontfx.i18n.PerobobbotDictionary;
 import perobobbot.frontfx.model.ApplicationIdentity;
 import perobobbot.frontfx.model.dialog.DefaultDialogHelper;
@@ -12,6 +15,7 @@ import perobobbot.frontfx.model.dialog.DialogHelper;
 import perobobbot.frontfx.model.dialog.DialogModel;
 import perobobbot.frontfx.model.state.ActionState;
 import perobobbot.frontfx.model.state.ApplicationState;
+import perobobbot.frontfx.model.state.Configuration;
 import perobobbot.frontfx.model.state.StyleState;
 import perobobbot.frontfx.model.view.EmptyFXView;
 import perobobbot.fx.FXDictionary;
@@ -20,13 +24,24 @@ import perobobbot.fx.KeyTracker;
 import perobobbot.fx.LocaleProperty;
 import perobobbot.fx.dialog.AlertShower;
 import perobobbot.fx.dialog.DefaultDialogPreparer;
+import perobobbot.fx.dialog.ValidatableField;
 import perobobbot.i18n.Dictionary;
 import perococco.perobobbot.frontfx.gui.style.StyleConfiguration;
 
+import java.net.URI;
+
 @SpringBootConfiguration
 @ComponentScan
+@PropertySources({
+        @PropertySource(value = "${app.config.dir}/server/application.properties", ignoreResourceNotFound = true),
+        @PropertySource("classpath:/configuration.properties")
+})
 @RequiredArgsConstructor
-public class PerobobbotGUI  {
+public class PerobobbotGUI {
+
+
+    @Value("${server.uri}")
+    private final URI serverUri;
 
     @NonNull
     private final StyleConfiguration styleConfiguration;
@@ -43,6 +58,7 @@ public class PerobobbotGUI  {
     public @NonNull ApplicationIdentity applicationIdentity() {
         return ApplicationIdentity.with(initialApplicationState());
     }
+
 
     @Bean
     @NonNull
@@ -78,7 +94,8 @@ public class PerobobbotGUI  {
     @Bean
     @NonNull
     public DialogHelper dialogHelper() {
-        return new DefaultDialogHelper(fxProperties,dictionary(),getDialogModel(),styleConfiguration.styleManager(),new DefaultDialogPreparer());
+        return new DefaultDialogHelper(fxProperties, dictionary(), getDialogModel(), styleConfiguration.styleManager(),
+                                       new DefaultDialogPreparer());
     }
 
     @NonNull
@@ -86,12 +103,17 @@ public class PerobobbotGUI  {
         return ApplicationState.builder()
                                .fxViewType(EmptyFXView.class)
                                .actionState(ActionState.allEnabled())
+                               .configuration(initialConfiguration())
                                .styleState(initialStyleState())
                                .build();
     }
 
+    private @NonNull Configuration initialConfiguration() {
+        return new Configuration(serverUri);
+    }
 
-    private StyleState initialStyleState() {
+
+    private @NonNull StyleState initialStyleState() {
         final var themes = styleConfiguration.getThemes();
         final var selectedTheme = styleConfiguration.getInitialTheme();
         return StyleState.builder()
