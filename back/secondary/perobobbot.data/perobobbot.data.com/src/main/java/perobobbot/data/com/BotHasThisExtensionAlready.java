a179ccc7ea8eefb60d3718b3c7ccd0856e0f6882
perococco <perococco.twitch@gmail.com>
2021-05-28 19:50:24 +0200

    Remove unused class

--- back/secondary/perobobbot.data/perobobbot.data.com/src/main/java/perobobbot/data/com/BotHasThisExtensionAlready.java
+++ back/secondary/perobobbot.data/perobobbot.data.com/src/main/java/perobobbot/data/com/BotHasThisExtensionAlready.java
@@ -1,13 +1,16 @@
 package perobobbot.data.com;
 
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.util.UUID;
 
 public class BotHasThisExtensionAlready extends DataException {
 
+    @Getter
     private final @NonNull UUID botId;
 
+    @Getter
     private final @NonNull UUID extensionId;
 
     public BotHasThisExtensionAlready(@NonNull UUID botId, @NonNull UUID extensionId) {
