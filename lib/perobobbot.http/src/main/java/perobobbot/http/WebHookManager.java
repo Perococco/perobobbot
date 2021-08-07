8eac37f667215612e18319969cfd96ae1c4c0ecd
Bastien Aracil <findbastien@gmail.com>
2021-08-07 18:28:14 +0200

    Complete the doc in README.asciidoc.

--- lib/perobobbot.http/src/main/java/perobobbot/http/WebHookManager.java
+++ lib/perobobbot.http/src/main/java/perobobbot/http/WebHookManager.java
@@ -16,7 +16,7 @@ public interface WebHookManager {
     /**
      * @param path the path of the URL the external server should call. The host is provided by this manager.
      * @param listener a listener the request from the server will be dispatched to.
-     * @return an optional containing the subscription that can be used to remove this subscription. It also contains the complete URL
+     * @return an optional containing the subscription that can be used to remove the listener. It also contains the complete URL
      * to pass to the external server when subscribing to a webhook to this server. If the webhook manager is disabled, return an empty
      * optional
      */
